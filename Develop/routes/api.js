20aea6c4c8e173fd319b837bde7244061c823a2b
Samantha Lord <samlord14@outlook.com>
2022-05-19 17:26:03 -0700

    updated app.js

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -21,24 +21,33 @@ class Store {
     fs.readFile('db/db.json', function (err, note){
         console.log(note.toString());
     });
-    // app.get('./', function (req, res) {
-    //     fs.readFile('db/db.json', 'utf8', function (err, data) {
-    //         if (err) {
-    //             res.send({error: err});
-    //         }
-    //         res.send(data);
-    //     })
-    // });
-    // //add Notes
-    // app.post('./', function (req, res) {
-    //     fs.readFile('./test.json', 'utf8', function (err, data) {
-    //         if (err) {
-    //             res.send({error: err});
-    //         }
-    //         res.send(data);
-    //     })
-    // });
-    // //delete notes
-
+     app.get('/api', function (req, res) {
+         fs.readFile('db/db.json', 'utf8', function (err, data) {
+             if (err) {
+                 res.send({error: err});
+             }
+             res.send(data);
+        })
+     });
+    //add Notes
+     app.post('/api', function (req, res) {
+         fs.writeFile('./test.json', 'utf8', function (err, data) {
+             if (err) {
+                 res.send({error: err});
+             }
+             res.send(data);
+         })
+     });
+     //delete notes
+     app.post('/api/:id', function (req, res) {
+         let noteList = [];
+        fs.writeFile('db/db.json', 'utf8', function (err, data) {
+            if (err) {
+                res.send({error: err});
+            }
+            res.json(noteList);
+        })
+    });
 
 //export a NEW instance of your class
+module.exports = app;
