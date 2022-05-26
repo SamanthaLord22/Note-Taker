f176c7e2fa95c5c306043060610fff7a8361a340
Samantha Lord <samlord14@outlook.com>
2022-05-26 09:21:25 -0700

    updated server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 //created server.js file
 const express = require('express');
 const htmlRoutes = require('./routes/html.js')
-
+const api = require("./routes/api")
 const app = express(); //added
 const PORT = process.env.PORT || 3001;
 app.use(express.static('public'));
@@ -13,7 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // GET Route for homepage
- app.use('/', htmlRoutes)
+ app.use(htmlRoutes)
+ app.use(api)
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
