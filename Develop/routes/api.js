bb25b8f7bc5afe9b49d032468186883f9b92d150
Samantha Lord <samlord14@outlook.com>
2022-05-26 09:18:27 -0700

    updated api.js

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -2,52 +2,67 @@ const util = require('util');
 const fs = require('fs');
 express = require('express');
 const app = express();
+const db = require("../db/db.json")
+
+// const uuid1 = require("uuid/v1");
 
 
-const uuid1 = require("uuid/v1")
+// const readFile = util.promisify(fs.readFile)
+// const writeFile = util.promisify(fs.writeFile)
 
-const readFile = util.promisify(fs.readFile)
-const writeFile = util.promisify(fs.writeFile)
+// class Store {
+//     readNotes() {
+//         return readFile("db/db.json", "utf8");
+//     }
+//     writeNotes(note) {
+//         return writeFile("db/db.json", JSON.stringify(note))
+//     }
+// }
+// //get notes(readNotes)
+// fs.readFile('db/db.json', function (err, note){
+//     console.log(note.toString());
+// });
+app.get('/api/notes', function (req, res) {
+    fs.readFileSync('db/db.json', 'utf8', function (err, data) {
+        if (err) {
+            res.send({ error: err });
+        }
+        db = JSON.parse(data)
+        res.json(db)
+    })
+});
+//add Notes
+app.post('/api/notes', function (req, res) {
+    db.push({
+        id: Math.floor(Math.random() * 999),
+        title: req.body.title,
+        text: req.body.text
+    })
+    fs.writeFileSync('db/db.json', JSON.stringify(db), function (err, data) {
+        if (err) {
+            res.send({ error: err });
+        }
 
-class Store {
-    readNotes() {
-        return readFile("db/db.json", "utf8");
+    })
+    res.json(db)
+});
+//delete notes
+app.delete('/api/notes/:id', function (req, res) {
+    let temparr = []
+    for (let i = 0; i < db.length; i++) {
+        if (db[i].id != req.params.id) {
+            temparr.push(db[i])
+        }
     }
-    writeNotes(note) {
-        return writeFile("db/db.json", JSON.stringify(note))
-    }
-}
-    // //get notes(readNotes)
-    fs.readFile('db/db.json', function (err, note){
-        console.log(note.toString());
-    });
-     app.get('/api', function (req, res) {
-         fs.readFile('db/db.json', 'utf8', function (err, data) {
-             if (err) {
-                 res.send({error: err});
-             }
-             res.send(data);
-        })
-     });
-    //add Notes
-     app.post('/api', function (req, res) {
-         fs.writeFile('./test.json', 'utf8', function (err, data) {
-             if (err) {
-                 res.send({error: err});
-             }
-             res.send(data);
-         })
-     });
-     //delete notes
-     app.post('/api/:id', function (req, res) {
-         let noteList = [];
-        fs.writeFile('db/db.json', 'utf8', function (err, data) {
-            if (err) {
-                res.send({error: err});
-            }
-            res.json(noteList);
-        })
-    });
+    db = temparr;
+    fs.writeFileSync('db/db.json', JSON.stringify(db), function (err, data) {
+        if (err) {
+            res.send({ error: err });
+        }
+
+    })
+    res.json(db)
+});
 
 //export a NEW instance of your class
 module.exports = app;
